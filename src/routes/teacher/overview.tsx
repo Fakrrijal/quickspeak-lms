616ada03d74b1444717dbc49c6d2ffbf2dfaa6e6
Fakrrijal <fakrrijal@gmail.com>
2026-09-11 22:59:16 +0700

    refactor(teacher): update dashboard v2 import

--- src/routes/teacher/overview.tsx
+++ src/routes/teacher/overview.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { TeacherDashboardV2 } from '../teacher-dashboard-v2'
+import { TeacherDashboardV2 } from '../../components/teacher/TeacherDashboardV2'
 
 export const Route = createFileRoute('/teacher/overview')({
   component: TeacherDashboardV2,
