d0bb81ab2fade7fb754d791299cd1c8428564e74
Fakrrijal <fakrrijal@gmail.com>
2026-09-13 04:04:03 +0700

    chore: keep sidebar views on existing learning route

--- src/routes/student/assessment.tsx
+++
@@ -1,4 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { StudentAssessmentPage } from '../../components/student/StudentAssessmentPage'
-
-export const Route = createFileRoute('/student/assessment')({ component: StudentAssessmentPage })
