999c07bb0f0324dbaf7ea4410679321bad8688b7
Fakrrijal <fakrrijal@gmail.com>
2026-09-15 12:42:45 +0700

    Test mobile payment picker return event handling

--- src/routes/student-payment.tsx
+++ src/routes/student-payment.tsx
@@ -1,6 +1,25 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useLayoutEffect } from 'react'
 import { StudentPaymentPageV3 } from '../components/student/StudentPaymentPageV3'
 
+function StudentPaymentRoute() {
+  useLayoutEffect(() => {
+    const blockPickerReturnHandlers = (event: Event) => {
+      event.stopImmediatePropagation()
+    }
+
+    window.addEventListener('focus', blockPickerReturnHandlers, true)
+    window.addEventListener('pageshow', blockPickerReturnHandlers, true)
+
+    return () => {
+      window.removeEventListener('focus', blockPickerReturnHandlers, true)
+      window.removeEventListener('pageshow', blockPickerReturnHandlers, true)
+    }
+  }, [])
+
+  return <StudentPaymentPageV3 />
+}
+
 export const Route = createFileRoute('/student-payment')({
-  component: StudentPaymentPageV3,
+  component: StudentPaymentRoute,
 })
