fa335baf456049c2bcaeb41299689d1a425ab5e1
Fakrrijal <fakrrijal@gmail.com>
2026-09-25 11:20:07 +0700

    feat(seo): define homepage route metadata

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { QuickSpeakLanding } from '../components/public/QuickSpeakLanding'
+import { createSeoHead } from '../lib/seo'
+
+const canonicalUrl = 'https://quickspeakindonesia.net/'
 
 export const Route = createFileRoute('/')({
+  head: () =>
+    createSeoHead({
+      title: 'Kursus Bahasa Inggris | QuickSpeak English Course',
+      description:
+        'QuickSpeak English Course menyediakan kursus Bahasa Inggris online dan di Bandung dengan pembelajaran terstruktur, 4 level, private dan semi-private.',
+      url: canonicalUrl,
+    }),
   component: HomePage,
 })
 
